6d0fedb22a838bd6b3696e37f91cf3db1d0743a5
gamersa22 <140577703+gamersa22@users.noreply.github.com>
2025-11-29 09:26:33 +1030

    update 1.3 - added Scaling

--- QuestMover.lua
+++ QuestMover.lua
@@ -1,11 +1,13 @@
 QuestMover = {}
 
 QuestMover.name = "QuestMover"
+QuestMover.VisualName = "Quest Mover"
 QuestMover.version = 1
 QuestMover.defaultCharacter = 
 {	
 	["offsetY"] = 325,
 	["offsetX"] = 0,
+	["scale"] = 1,
 	["useCharacterSettings"] = false,
 }
 QuestMover.default = {
@@ -30,7 +32,29 @@ function QuestMover.ApplyGoldenAnchor()
 	ZO_PromotionalEventTracker_TL:ClearAnchors()
 	ZO_PromotionalEventTracker_TL:SetAnchor(BOTTOMRIGHT, ZO_ZoneStoryTracker, BOTTOMRIGHT,0,151)
 end
+function QuestMover.enableInheritScaleRecursive(control)
+    if not control then return end
+    if not control:GetInheritsScale() then control:SetInheritScale(true) end
+    local numChildren = control:GetNumChildren()
+    for i = 1, numChildren do
+        local child = control:GetChild(i)
+        if child then
+            QuestMover.enableInheritScaleRecursive(child)
+        end
+    end
+end
 
+---Applies scale transform to Quest Tracker (ty DakJaniels)
+function QuestMover.applyScale(controlToScale, scale)
+    if not controlToScale then return end
+    local appliedScale = scale or 1
+    QuestMover.enableInheritScaleRecursive(controlToScale);
+    controlToScale:SetTransformScale(appliedScale);
+end
+function QuestMover.applyScales()
+	QuestMover.applyScale(ZO_FocusedQuestTrackerPanelContainer,QuestMover.GetSettings().scale)
+	QuestMover.applyScale(ZO_ZoneStoryTrackerContainer,QuestMover.GetSettings().scale)
+end
 local QuestTrackerInMenu = false
 function QuestMover.Initialize()
 	--Load up, those Saved Vars
@@ -42,9 +66,10 @@ function QuestMover.Initialize()
 	ZO_FocusedQuestTrackerPanelContainer:SetHandler("OnShow",function() ZoneStoryQuest=false QuestMover.ApplyGoldenAnchor() end)
 	ZO_ZoneStoryTrackerContainer:SetHandler("OnShow",function()ZoneStoryQuest=true QuestMover.ApplyGoldenAnchor() end)
 	QuestMover.ApplyGoldenAnchor()
-	
+	QuestMover.applyScales()
     local LHAS = LibHarvensAddonSettings
-    
+
+
     local options = {
         allowDefaults = true,
 		allowRefresh = false,
@@ -53,7 +78,7 @@ function QuestMover.Initialize()
         end,
     }
     
-    local settings = LHAS:AddAddon("Quest Mover", options)
+    local settings = LHAS:AddAddon(QuestMover.VisualName, options)
     if not settings then
         return
     end
@@ -72,9 +97,7 @@ function QuestMover.Initialize()
     settings:AddSetting(checkbox)
 	local scene
 	local function addQT()
-		if QuestTrackerInMenu then
-			return
-		end
+		if QuestTrackerInMenu then return end
 		scene = SCENE_MANAGER:GetCurrentScene()					
 		if ZoneStoryQuest then
 			scene:AddFragment(ZONE_STORY_TRACKER_FRAGMENT)
@@ -90,8 +113,7 @@ function QuestMover.Initialize()
 	end
 	
 	local function addonSelected(_, addonSettings)
-		local addQT = addonSettings == settings
-		if not addQT and QuestTrackerInMenu then	
+		if QuestTrackerInMenu then	
 			scene:RemoveFragment(ZONE_STORY_TRACKER_FRAGMENT)
 			ZONE_STORY_TRACKER_FRAGMENT:Refresh()
 			scene:RemoveFragment(FOCUSED_QUEST_TRACKER_FRAGMENT)
@@ -102,9 +124,9 @@ function QuestMover.Initialize()
 		
 	CALLBACK_MANAGER:RegisterCallback("LibHarvensAddonSettings_AddonSelected", addonSelected)
 
-	local button = {
+	settings:AddSetting({
         type = LHAS.ST_BUTTON,
-        label = "Show Quest Tracker NOW",		
+        label = "Show Quest Tracker",		
         buttonText = "Show",
         clickHandler = function(control, button)
 			if not QuestTrackerInMenu then
@@ -113,29 +135,12 @@ function QuestMover.Initialize()
 				addonSelected()
 			end
         end,
-    }
-	settings:AddSetting(button)
-	
-	--Slider to Adjust Quest Traker's Y Position
-    local slider = {
-        type = LHAS.ST_SLIDER,
-        label = "Up <- -> Down",
-		tooltip = "Default: 325 \nBase Game: 100",
-        setFunction = function(value)
-            QuestMover.GetSettings().offsetY = value
-			QuestMover.ApplyAnchor()
-        end,
-        getFunction = function()
-            return QuestMover.GetSettings().offsetY
-        end,
-        --default = 100,
-        min = 0,
-        max = 900,
-        step = 5
-    }
-    settings:AddSetting(slider)
-	--X value
-	 local slider = {
+    })	
+	local maxX, maxY = GuiRoot:GetDimensions()
+	maxX = math.floor(maxX)
+	maxY = math.floor(maxY)
+	--Slider to Adjust Quest Traker's X Position
+	 settings:AddSetting({
         type = LHAS.ST_SLIDER,
         label = "Left <- -> Right",
 		tooltip = "Default is 0",
@@ -146,22 +151,54 @@ function QuestMover.Initialize()
         getFunction = function()
             return QuestMover.GetSettings().offsetX
         end,
-        --default = 100,
-        min = -1650,
+        default = QuestMover.defaultCharacter.offsetX,
+        min = -maxX,
         max = 0,
         step = 5
-    }
-    settings:AddSetting(slider)
-	local checkbox = {
-		type = LHAS.ST_BUTTON,
-        label = "Report a Bug",
-        tooltip = "Open a thread to report bugs specifically with the console version of Quest Mover. Please check to make sure the issue hasn't been reported yet.",
-        buttonText = "Open URL",
-        clickHandler = function(control, button)
-			RequestOpenUnsafeURL("https://www.esoui.com/forums/showthread.php?t=11316")
+    })
+	--Slider to Adjust Quest Traker's Y Position
+    settings:AddSetting({
+        type = LHAS.ST_SLIDER,
+        label = "Up <- -> Down",
+		tooltip = "Default: 325 \nBase Game: 100",
+        setFunction = function(value)
+            QuestMover.GetSettings().offsetY = value
+			QuestMover.ApplyAnchor()
+        end,
+        getFunction = function()
+            return QuestMover.GetSettings().offsetY
+        end,
+        default = QuestMover.defaultCharacter.offsetY,
+        min = 0,
+        max = maxY,
+        step = 5
+    })
+	--Slider to Adjust Quest Traker's Scale
+    settings:AddSetting({
+        type = LHAS.ST_SLIDER,
+        label = "Small <- -> Large",
+		tooltip = "Default: 1",
+        setFunction = function(value)
+            QuestMover.GetSettings().scale = value
+			QuestMover.applyScales()
+        end,
+        getFunction = function()
+            return QuestMover.GetSettings().scale
+        end,
+        default = QuestMover.defaultCharacter.scale,
+        min = 0.5,
+        max = 1.5,
+        step = 0.05
+    })
+	settings:AddSetting({
+        type = LHAS.ST_BUTTON,
+        label = "Submit Feedback / Request",
+		tooltip = "link to a form where you can leave feedback or even leave a request",
+		buttonText = "Open URL",
+		clickHandler = function(control, button)
+			RequestOpenUnsafeURL("https://docs.google.com/forms/d/e/1FAIpQLScYWtcIJmjn0ZUrjsvpB5rwA5AlsLvasHUIcKqzIYcogo9vjQ/viewform?usp=pp_url&entry.550722213="..QuestMover.VisualName)
 		end,
-    }
-    settings:AddSetting(checkbox)
+	})
 end
 function QuestMover.OnAddOnLoaded(event, addonName)
 	if addonName == QuestMover.name then
@@ -173,4 +210,3 @@ end
 EVENT_MANAGER:RegisterForEvent(QuestMover.name, EVENT_ADD_ON_LOADED, QuestMover.OnAddOnLoaded)
 
 --HUGE thanks to Dolgubon for helping me working out on how to do this
-
